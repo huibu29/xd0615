4141dfebed8987bd06065b51ebd92be8973763fc
huibu29 <k2303538438@outlook.com>
2020-06-19 18:51:34 +0800

    第三次作业

---
+++ doubleMotor/doubleMotor.c
@@ -0,0 +1,56 @@
+char ch=' ';
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(6,OUTPUT);
+  pinMode(7,OUTPUT);
+  pinMode(8,OUTPUT);
+  pinMode(9,OUTPUT);
+  Serial.println("Link Start");
+}
+
+/*
+预定
+'f':表示前进
+'b':表示停止
+'r':表示右转
+'l':表示左转
+*/
+void loop()
+{
+  if(Serial.available()>0)
+  {
+  	ch=Serial.read();
+    switch(ch)
+    {
+    case 'f':
+      digitalWrite(6,HIGH);
+      digitalWrite(7,LOW);
+      digitalWrite(8,HIGH);
+      digitalWrite(9,LOW);
+      break;
+    case 'b':
+      digitalWrite(7,HIGH);
+      digitalWrite(6,LOW);
+      digitalWrite(9,HIGH);
+      digitalWrite(8,LOW);
+      break;
+    case 'r':
+      digitalWrite(7,HIGH);
+      digitalWrite(6,LOW);
+      digitalWrite(8,HIGH);
+      digitalWrite(9,LOW);
+      break;
+    case 'l':
+      digitalWrite(6,HIGH);
+      digitalWrite(7,LOW);
+      digitalWrite(9,HIGH);
+      digitalWrite(8,LOW);
+      break;
+    
+    default:
+      break;
+    }
+  }
+}
+
